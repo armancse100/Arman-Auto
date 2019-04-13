062ff064d72faf33b63a2651df71171d8df3ab9d
Arman Ullah <arman.ullah@outlook.com>
2019-04-13 14:15:16 -0500

    Add project files.

---
+++ ArmanAuto/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ArmanAuto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
