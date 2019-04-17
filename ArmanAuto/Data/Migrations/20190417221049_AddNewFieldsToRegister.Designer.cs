a2136629581382fee9187c71364ee7aa2ea8c514
Arman Ullah <arman.ullah@outlook.com>
2019-04-17 17:11:47 -0500

    Database is updated by adding new columns for the Register

--- ArmanAuto/Data/Migrations/20190417221049_AddNewFieldsToRegister.Designer.cs
+++ ArmanAuto/Data/Migrations/20190417221049_AddNewFieldsToRegister.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace ArmanAuto.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190417221049_AddNewFieldsToRegister")]
+    partial class AddNewFieldsToRegister
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
